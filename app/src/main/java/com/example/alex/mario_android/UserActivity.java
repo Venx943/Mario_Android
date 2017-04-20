8ee585e15478f21282dcd0d3a5c42006a29dce63
Fluidmars <durso54@gmail.com>
2017-04-20 16:53:21 +0200

    Cambiato il nome da 'MainActivity' a 'UserActivity'

--- app/src/main/java/com/example/alex/mario_android/UserActivity.java
+++ app/src/main/java/com/example/alex/mario_android/UserActivity.java
@@ -3,7 +3,7 @@ package com.example.alex.mario_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
