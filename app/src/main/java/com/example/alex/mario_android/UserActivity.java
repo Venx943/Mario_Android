ffa2d1e19fa05a3b05e8b2e6c1959985b650e986
Fluidmars <durso54@gmail.com>
2017-04-26 09:47:57 +0200

    Aggiunta del 'setOnClickListener' per l'accesso all'activity Prenota

--- app/src/main/java/com/example/alex/mario_android/UserActivity.java
+++ app/src/main/java/com/example/alex/mario_android/UserActivity.java
@@ -1,7 +1,10 @@
 package com.example.alex.mario_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
+    /**Apertura di una nuova activity quando viene premuto il tasto prenota**/
+
+    public void Prenota(View view) {
+        Button btnPrenota;
+        btnPrenota = (Button) findViewById(R.id.book_button);
+        /**Apertura di una nuova Activity al Click del bottone Prenota**/
+        btnPrenota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent prenotazioni = new Intent(UserActivity.this, Prenota.class);
+                startActivity(prenotazioni);
+            }
+        });
     }
 }
